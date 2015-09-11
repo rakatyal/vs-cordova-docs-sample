8b85ebcdbeeac6fb5553f4b54ae1cd2340b66389
Raghav Katyal <rakatyal@microsoft.com>
2015-09-11 13:01:59 -0700

    Gitignore

---
+++ dps-web-experience-sample/Models/License.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACOM.DocumentationSample.Models
+{
+    public class License
+    {
+        public string PageTitle { get; set; }
+    }
+}
