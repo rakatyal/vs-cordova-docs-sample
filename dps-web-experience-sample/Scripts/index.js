06e3908adee3aef495ab254ddc38dfbf208284db
Linda Zhong <lizhong@microsoft.com>
2015-09-17 16:44:14 -0700

    TOC edits, last time or else I'm just a bad coder
    3 errors, same as before

--- dps-web-experience-sample/Scripts/index.js
+++ dps-web-experience-sample/Scripts/index.js
@@ -23,7 +23,7 @@
     "package-app-built-with-visual-studio": { parent: "package_and_publish" }, "tutorial-package-publish-readme": { parent: "package_and_publish" },
     "get-your-app-approved": { parent: "package_and_publish" }, "publish-app-built-with-visual-studio": { parent: "package_and_publish" },
 
-    "tutorial-ionic": { parent: "tutorials_and_samples" }, "O365_Files": { parent: "tutorials_and_samples" }, "O365_Ionic": { parent: "tutorials_and_samples" },
+    "create-to-do-list-app": { parent: "tutorials_and_samples" }, "tutorial-ionic": { parent: "tutorials_and_samples" }, "o365_files": { parent: "tutorials_and_samples" }, "o365_ionic": { parent: "tutorials_and_samples" },
     "convert_cordova_phonegap": { parent: "tutorials_and_samples" },
 
     "tips-and-workarounds-general-readme": { parent: "tips_and_workarounds" }, "tips-and-workarounds-android-readme": { parent: "tips_and_workarounds" },
